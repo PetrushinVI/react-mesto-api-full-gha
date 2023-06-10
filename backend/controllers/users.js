74547d3031269cdcce42e7c627fe97473c05eb76
Владислав <alina220578@mail.ru>
2023-06-10 21:47:02 +0300

    pr15.1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -140,6 +140,7 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  console.log(process.env.NODE_ENV);
   userSchema
     .findUserByCredentials(email, password)
     .then((user) => {
